d1f0676d4d4036d2dd97a56086f9db2ba82ec306
gotoa1234 <cap8825@gmail.com>
2022-11-29 21:31:13 +0800

    1. SelectionSort Finish

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,9 @@ namespace Algorithm_Sort
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            var selectionExecute = new SelectionSortExecute();
+            selectionExecute.Execute();
+
             var bucketExecute = new BucketSortExecute();
             bucketExecute.Execute();
 
--- NotPractical/SelectionSort.cs
+++ NotPractical/SelectionSort.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Algorithm_Sort.NotPractical
+﻿namespace Algorithm_Sort.NotPractical
 {
     public class SelectionSortExecute
     {
@@ -25,8 +19,8 @@ namespace Algorithm_Sort.NotPractical
     ///        Space : O(1)
     ///    Best Time : O(n²)
     ///   Worst Time : O(n²)
-    ///         原理 : 
-    ///                
+    ///         原理 : 泡沫排序的變形，但不用頻繁交換，每次找出最小值才做交換
+    ///                雖然是時間複雜度O(n²)但效能會比泡沫好很多，因為實際交換次數為n次
     /// </summary>
     public class SelectionSort<T> where T : IComparable
     {
@@ -37,7 +31,29 @@ namespace Algorithm_Sort.NotPractical
         /// <returns>倒序的陣列 EX: [5,4,3,2,1]</returns>
         public List<T> SelectionAscendingSort(List<T> items)
         {
+            int index = 0;
+            //1. 遍歷 
+            for (int keyIndex = 0; keyIndex < items.Count() - 1; keyIndex++)
+            {
+                //2. 找出最小值 
+                var minIndex = keyIndex + 1;
+                for (index = keyIndex + 1; index < items.Count; index++)
+                {                    
+                    if (items[index].CompareTo(items[keyIndex]) < 0 &&
+                        items[index].CompareTo(items[minIndex]) < 0)
+                    {
+                        minIndex = index;
+                    }
+                }
+                //3. 如果當前keyIndex 已經是最小值則不用交換
+                if (items[keyIndex].CompareTo(items[minIndex]) < 0)
+                    continue;
 
+                //4. 發現最小值進行minIndex 與 keyIndex交換
+                var temp = items[keyIndex];
+                items[keyIndex] = items[minIndex];
+                items[minIndex] = temp;
+            }
             return items;
         }
     }
