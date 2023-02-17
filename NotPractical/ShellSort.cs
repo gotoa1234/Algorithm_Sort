b05d4ff9abeb534b421bd10e72d2164735277f89
gotoa1234 <cap8825@gmail.com>
2023-02-17 09:49:14 +0800

    1. shell sort finish

--- NotPractical/ShellSort.cs
+++ NotPractical/ShellSort.cs
@@ -26,7 +26,18 @@ namespace Algorithm_Sort.NotPractical
     ///        Space : O(1)
     ///    Best Time : O(nlong)
     ///   Worst Time : O(n²)
-    ///         原理 : 
+    ///         原理 : 設定一個自定義間隔數(interval)，然後數列中間隔數之間進行比較，小的進行交換
+    ///                然後每次縮減間隔數，直到最後排序完成
+    ///    不穩定原因：最壞的情況下，會是插入排序演算法(遍歷X 比較 X-1)，但運氣好在間隔數未縮減的情況下剛好排序完成    
+    ///    演算步驟(Interval = 3 ; 總元素 = 9):    
+    ///     
+    ///     STEP   1:  92, 17, 38, 59, 26, 39, 41, 75, 6   => 一開始 Array[4] 與 Array[0] 比較 ，進行交換
+    ///     STEP 2-1:  26, 17, 38, 59,  6, 39, 41, 75, 92  => 第二輪 Array[8] 與 Array[4] 比較，進行交換
+    ///     STEP 2-2:   6, 17, 38, 59, 26, 39, 41, 75, 92  =>   繼續 Array[4] 與 Array[0] 比較，進行交換
+    ///     STEP 3-1:   6, 17, 38, 59, 26, 39, 41, 75, 92  => 第三輪 gap /= 3 縮減間隔(gap = 1) 開始進行比較，此時為插入排序，遇到26插入到前方適合位置
+    ///     STEP 3-2:   6, 17, 26, 38, 59, 39, 41, 75, 92  =>   繼續 39插入到合適位置  
+    ///     STEP 3-3:   6, 17, 26, 38, 39, 59, 41, 75, 92  =>   繼續 41插入到合適位置
+    ///     STEP 3-4:   6, 17, 26, 38, 39, 41, 59, 75, 92  =>   直到92都沒有需要插入，所以排序完成
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class ShellSort<T> where T : IComparable
@@ -56,28 +67,4 @@ namespace Algorithm_Sort.NotPractical
             return items;
         }
     }
-
-//    int n = arr.Length;
-
-//    // 選擇希爾序列
-//    int gap = 1;
-//    while (gap<n / 3)
-//    {
-//        gap = 3 * gap + 1;
-//    }
-
-//while (gap >= 1)
-//{
-//    for (int i = gap; i < n; i++)
-//    {
-//        for (int j = i; j >= gap && arr[j] < arr[j - gap]; j -= gap)
-//        {
-//            // 交換元素位置
-//            int temp = arr[j];
-//            arr[j] = arr[j - gap];
-//            arr[j - gap] = temp;
-//        }
-//    }
-//    gap /= 3;
-//}
 }
